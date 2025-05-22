35bbdc98c52d9bec53e0cc9c35c67d4880a2c556
rohan03-stack <ronygaikwad03@gmail.com>
2025-05-22 16:42:28 +0530

    index2 is added

---
+++ node/day4/index2.js
@@ -0,0 +1,33 @@
+const http = require("http");
+
+const fs = require("fs");
+
+const port  = 8000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+    
+    res.setHeader("Content-type", "text/html");
+    res.end("<h1>Hey we have the value!</h1>");
+  } else if (req.url == "/notes") {
+ 
+    res.write("here we have the value of the something.");
+    res.end();
+  } else if (req.url == "/note") {
+   fs.readFile("./index1.js", {encoding: "utf-8"}, (err,data) => {
+   if(err) {
+    res.end(err);
+   }else{
+    res.end(data);
+   }
+   })
+   
+  } else {
+    res.end("Invalid API endpoints, Please refer API docs...");
+  }
+});
+
+
+server.listen(port, () => {
+  console.log("server running on", port);
+});
