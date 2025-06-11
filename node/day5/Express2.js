3db979ac456e21dcaaad58568c6e9c6072541bb9
rohan03-stack <ronygaikwad03@gmail.com>
2025-06-11 13:28:09 +0530

    express 2 is added

--- node/day5/Express2.js
+++ node/day5/Express2.js
@@ -1,26 +1,52 @@
+// const express = require('express');
+
+// const fs =require('fs');
+
+// const Port = 8000;
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.send('add data...')
+// })
+
+// app.post('/students', (req, res) => {
+//     fs.writeFileSync('db.json', JSON.stringify(req.body), 'utf-8')
+//     res.send('accepted data...')
+// })
+
+// app.listen(Port, () => {
+//     console.log('port was listening', Port);
+    
+// })
+
+//   ..........................................read......................................
 const express = require('express');
 
 const fs =require('fs');
 
-const Port = 8000;
+const Ports = 8500;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.text());
 
-app.get('/', (req, res) => {
-    res.send('add data...')
+app.get('/shoeData', (req, res) => {
+  const show = fs.readFile('./students.txt', {encoding: 'utf-8'})
 })
 
 app.post('/students', (req, res) => {
-    fs.writeFileSync('db.json', JSON.stringify(req.body), 'utf-8')
+    fs.writeFileSync(students.txt) , req.body, 'utf-8';
     res.send('accepted data...')
 })
 
 app.listen(Port, () => {
-    console.log('port was listening', Port);
+    console.log('port was listening', Ports);
     
 })
+
 
 
 
